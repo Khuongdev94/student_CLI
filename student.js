ee9b3dcb0310873b3593a973535c9a1755718866
khuong <khuonghumg1376@gmail.com>
2023-08-10 00:18:51 +0700

    add functions for student.js

--- student.js
+++ student.js
@@ -10,33 +10,75 @@ function loadData() {
 function showOption() {
   console.log("1. show students");
   console.log("2. create student");
-  console.log("1. save & exit");
+  console.log("3. save & exit");
+  console.log("4. delete student");
+  console.log("5. edit student");
+
+  const input = readlineSync.question(" > ");
+  switch (input) {
+    case "1":
+      showStudents();
+      showOption();
+      break;
+    case "2":
+      addStudents();
+      showOption();
+      break;
+    case "3":
+      saveAndExit();
+      break;
+    case "4":
+      deleteStudent();
+      break;
+    case "5":
+      editStudent();
+      showOption();
+      break;
+    default:
+      console.log("wrong input");
+      showOption();
+      break;
+  }
 }
 
-function showStudents() {}
+function showStudents() {
+  for (let student of students) {
+    console.log(student.name, student.age);
+  }
+}
+3;
 
-function addStudents() {}
+function addStudents() {
+  const name = readlineSync.question("name: ");
+  const age = readlineSync.question("age: ");
+  const student = { name: name, age: Number(age) };
+  students.push(student);
+}
 
-function saveAndExit() {}
+function saveAndExit() {
+  const addData = JSON.stringify(students);
+  fs.writeFileSync("./data.json", addData, { encoding: "utf-8" });
+}
+function editStudent() {
+  const editStudent = readlineSync.question("nameEdit: ");
+  const updateStudent = students.find((item) => item.name === editStudent);
+  updateStudent.name = readlineSync.question("nameUpdate: ");
+  const age = readlineSync.question("ageUpdate: ");
+  updateStudent.age = Number(age);
+  saveAndExit();
+}
+
+function deleteStudent() {
+  const deleteStudent = readlineSync.question("nameDelete: ");
+  const itemDelete = students.find((item) => item.name === deleteStudent);
+  students.splice(students.indexOf(itemDelete), 1);
+  saveAndExit();
+}
 
 function run() {
   loadData();
-  console.log(students);
-  //   showOption();
-  //   const input = readlineSync.question(" > ");
-  //   switch (input) {
-  //     case "1":
-  //       showStudents();
-  //       break;
-  //     case "2":
-  //       addStudents();
-  //       break;
-  //     case "3":
-  //       saveAndExit();
-  //       break;
-  //     default:
-  //       console.log("wrong input");
-  //       break;
-  //   }
+  showOption();
+
+  saveAndExit();
 }
 run();
